a57ec396489cbc20f3c1dc08a6a1821b7b071061
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2023-09-22 08:11:27 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,24 @@
-from pynput.keyboard import Key, Controller
 import time
+from pynput.keyboard import Key, Controller
+import random
 
-message = "Flod para whats app em python d"
+messages = ["eai silas malafaio", "tem algum boca aberta aqui?", "eai caraguatatubo", "que isso delicio", "fica bravo não cnpjoto"
+            , "eai sandalio", "eai ludmilo", "que isso claudio arraio", "aqui é peri", "eai margarido", "salve gasolino", "eai tigreso"
+            , "eai calabreso", "salve prexeco", "eai trembolono", "fala creatino", "fala lombrigo", "eai princeso", "eai aeromoço",
+            "que iso prostituto", "me arrebente delicio", "que isso lesmo", "eai macaxeiro", "eai borboleto", "eai tadalafilo", 
+            "eai xoxoto", "fica bravo não sonho de valso", "que isso carniço", "se der 5 minutos em mim", "eai lesbico", "valeu ai glutamino", "eai asthetico"
+            "aqui é gueto", "o dilatafilo", "pé de breque", "eai lacraio", "mo cara de boca aberta mano", "aqui é quebrada irmao, aqui é peri", 
+            "aqui é pitango", "eai jiboio"]
+
 keyboard = Controller()
 
 time.sleep(5)
 
-for num in range(1000):
-    for letter in message:
-        keyboard.press(letter)
-        keyboard.release(letter)
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
-    time.sleep(0.1)
+for num in range(200):
+  var = random.randint(0, len(messages) -1)
+  for letter in messages[var]:
+    keyboard.press(letter)
+    keyboard.release(letter)
+  keyboard.press(Key.enter)
+  keyboard.release(Key.enter)
+  time.sleep(0.1)
